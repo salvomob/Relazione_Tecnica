d2a0e6835d12ee4a2a927a29223afc83a67e17fa
salvomob <salvo.mob@hotmail.it>
2021-06-02 18:16:53 +0200

    changes committed

--- yt_downloader.py
+++ yt_downloader.py
@@ -1,5 +1,10 @@
 from pytube import YouTube
-video_url = input("inserisci qui l'url del video che desideri scaricare : ") # YouTube video URL
-youtube = YouTube(video_url)
-video = youtube.streams.first()
-video.download("/home/salvatore/Scrivania")
+path = "/home/salvatore/Scrivania" #in here specify the path in which you want to download the video 
+url = input("inserisci ulr del video da scaricare : ")
+my_video = YouTube(url)
+print("nome video in fase di download:")
+print(my_video.title)
+print("in fase di downolad :), aspetta ancora un po'! ")
+my_video = my_video.streams.get_highest_resolution()
+my_video.download(path)
+print("Video scaricato in : " + path)
